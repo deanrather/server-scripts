91d60bc128e56643f15436a28a8d18981becfb1d
Dean Rather <deanrather@gmail.com>
2012-07-25 19:52:59 +1000

    wip git-pull script

--- git-pull.sh
+++ git-pull.sh
@@ -1,5 +1,17 @@
+#!/bin/bash
 
-#Change to working git repository to pull changes from bare repository
-cd /home/ubuntu/www/gamehub || exit
+# Check that a parameter was passed in
+if [ "$1" = "" ]
+then
+        echo -e "you must pass in the path to the git repo"
+        exit 1
+fi
+
+echo -e "changing working directory to $1";
+cd $1 || exit
+
+echo -e "unsetting git dir"
 unset GIT_DIR
+
+echo -e "updating from origin"
 git pull origin master
